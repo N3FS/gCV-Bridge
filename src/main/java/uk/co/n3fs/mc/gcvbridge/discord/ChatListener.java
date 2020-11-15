cc9d016df55efd68806d04c50cbf8932c976b3a9
MD <1917406+mdcfe@users.noreply.github.com>
2020-11-15 19:22:08 +0000

    Use MCDiscordReserializer for Discord -> MC messages

--- src/main/java/uk/co/n3fs/mc/gcvbridge/discord/ChatListener.java
+++ src/main/java/uk/co/n3fs/mc/gcvbridge/discord/ChatListener.java
@@ -2,7 +2,9 @@ package uk.co.n3fs.mc.gcvbridge.discord;
 
 import com.vdurmont.emoji.EmojiParser;
 import com.velocitypowered.api.proxy.ProxyServer;
+import dev.vankka.mcdiscordreserializer.minecraft.MinecraftSerializer;
 import me.lucko.gchat.api.ChatFormat;
+import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.event.ClickEvent;
 import net.kyori.adventure.text.event.HoverEvent;
@@ -14,59 +16,66 @@ import uk.co.n3fs.mc.gcvbridge.GCVBridge;
 
 public class ChatListener {
 
-    private final GCVBridge plugin;
-    private final ProxyServer proxy;
-
     private static final LegacyComponentSerializer LEGACY_LINKING_SERIALIZER = LegacyComponentSerializer.builder()
             .character('&')
             .extractUrls()
             .build();
+    private final GCVBridge plugin;
+    private final ProxyServer proxy;
 
     public ChatListener(GCVBridge plugin, ProxyServer proxy) {
         this.plugin = plugin;
         this.proxy = proxy;
     }
 
+    private static String replacePlaceholders(String format, MessageAuthor author, String message) {
+        return format == null ? null : format
+                .replace("{name}", author.getName())
+                .replace("{username}", author.getName())
+                .replace("{display_name}", author.getDisplayName())
+                .replace("{server_name}", "discord")
+                .replace("{message}", message);
+    }
+
+    private static TextComponent formatMessage(String format, MessageAuthor author, String message) {
+        if (format == null) return Component.empty();
+        String replaced = replacePlaceholders(format, author, "{message}");
+        Component convertedMessage = MinecraftSerializer.INSTANCE.serialize(message);
+        return Component.text("").append(LEGACY_LINKING_SERIALIZER.deserialize(replaced).replaceText("{message}", convertedMessage));
+    }
+
     public void onMessage(MessageCreateEvent event) {
-        if (plugin.getConfig().isPlayerlistEnabled() && event.getMessage().getReadableContent().toLowerCase().startsWith("playerlist")) return;
+        if (plugin.getConfig().isPlayerlistEnabled() && event.getMessage().getReadableContent().toLowerCase().startsWith("playerlist"))
+            return;
         if (!plugin.getConfig().getInChannels(event.getApi()).contains(event.getChannel())) return;
         if (event.getMessageAuthor().isYourself()) return;
+
+        MessageAuthor author = event.getMessageAuthor();
 
         ChatFormat format = plugin.getConfig().getInFormat(plugin.getGChatApi());
         String message = event.getReadableMessageContent();
         message = EmojiParser.parseToAliases(message);
-        MessageAuthor author = event.getMessageAuthor();
 
-        String formattedMsg = replacePlaceholders(format.getFormatText(), author, message);
-        String hover = replacePlaceholders(format.getHoverText(), author, message);
-
+        Component hover = formatMessage(format.getHoverText(), author, message);
         ClickEvent.Action clickType = format.getClickType();
         String clickValue = replacePlaceholders(format.getClickValue(), author, message);
 
-        TextComponent component = LEGACY_LINKING_SERIALIZER.deserialize(formattedMsg).toBuilder()
-            .applyDeep(m -> {
-                if (hover != null) {
-                    m.hoverEvent(HoverEvent.showText(LegacyComponentSerializer.legacy('&').deserialize(hover)));
-                }
-                if (clickType != null) {
-                    m.clickEvent(ClickEvent.clickEvent(clickType, clickValue));
-                }
-            })
-            .build();
+        TextComponent component = formatMessage(format.getFormatText(), author, message)
+                .toBuilder()
+                .applyDeep(m -> {
+                    if (hover != null) {
+                        m.hoverEvent(HoverEvent.showText(hover));
+                    }
+                    if (clickType != null) {
+                        m.clickEvent(ClickEvent.clickEvent(clickType, clickValue));
+                    }
+                })
+                .build();
 
         proxy.getAllPlayers().stream()
-            .filter(player -> !plugin.getConfig().isRequireSeePerm() || player.hasPermission("gcvb.see"))
-            .forEach(player -> player.sendMessage(component));
+                .filter(player -> !plugin.getConfig().isRequireSeePerm() || player.hasPermission("gcvb.see"))
+                .forEach(player -> player.sendMessage(component));
 
         plugin.getLogger().info(PlainComponentSerializer.plain().serialize(component));
-    }
-
-    private static String replacePlaceholders(String format, MessageAuthor author, String message) {
-        return format == null ? null : format
-            .replace("{name}", author.getName())
-            .replace("{username}", author.getName())
-            .replace("{display_name}", author.getDisplayName())
-            .replace("{server_name}", "discord")
-            .replace("{message}", message);
     }
 }
